e3a2f9a4b8f2353bbb67bfdb54712e3122043522
Slav91 <povst91@gmail.com>
2018-12-16 13:13:49 +0000

    Project Update

---
+++ MyProject/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timer = 5f;
+    public float countdown = 2f;
+
+    private int waveIndex = 0;
+    public Text waveCountdown;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timer;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdown.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
